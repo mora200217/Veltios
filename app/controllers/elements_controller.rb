02fc44580f9330f7304af2a681c3c8bf7627ddaf
Andres Morales <mora200217@hotmail.com>
2017-08-06 13:36:59 -0500

    Elements controller changed

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -1,4 +1,6 @@
 class ElementsController < ApplicationController
   def index
+    @elements = Element.all # Access to all elements in db
+    @reserve_item = current_reserve.reserve_items.new # Access to current reserve items and insert one 
   end
 end
