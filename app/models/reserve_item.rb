c3582ad80c310d2377f17bc528abba823f039f40
Andres Morales <mora200217@hotmail.com>
2017-08-06 17:15:54 -0500

    Reservve Item Model Changed. Finalize method and private validations inserted

--- app/models/reserve_item.rb
+++ app/models/reserve_item.rb
@@ -3,11 +3,32 @@ class ReserveItem < ApplicationRecord
   belongs_to :element
   belongs_to :reserve
 
+  # Initial Validations
+  validate :element_present
+  validate :reserve_present
+
+  # Invoke function before saving
+  before_save :finalize
+
   # Function Element Present
+  private
   def element_present
     # Check for Element Existance
     if element.nil?
-      errors.add(:element, "Is not a valid element or is inactive") # Display Erros Message
+      errors.add(:element, "Is not a valid element or is inactive") # Display Errors Message
     end
   end
+
+   # Function Reservation Present
+   def reserve_present
+  # Check for Reservation Existance
+    if reserve.nil? # Check for reserve type nil?
+      errors.add(:reserve, "Its not a valid reservation") # Display Errors Message
+    end
+   end
+
+# Finalize function
+   def finalize
+     self[:amount] = amount
+   end
 end
