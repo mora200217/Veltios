f15ba28b9f481cd0025c4e5ab601dc4efa3b3fd7
Andres Morales <mora200217@hotmail.com>
2017-08-06 17:08:11 -0500

    Added  reserve status reference in Reserve migration

--- db/migrate/20170806170752_create_reserves.rb
+++ db/migrate/20170806170752_create_reserves.rb
@@ -2,6 +2,7 @@ class CreateReserves < ActiveRecord::Migration[5.1]
   def change
     create_table :reserves do |t|
       t.integer :total
+      t.references :reserve_status, foreign_key: true
 
       t.timestamps
     end
