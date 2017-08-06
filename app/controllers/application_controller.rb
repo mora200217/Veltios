8bf0e3279561feff8a1b8c6ba21c50e2bf4b7419
Andres Morales <mora200217@hotmail.com>
2017-08-06 13:18:06 -0500

    Application Controller modified. Check for current reserve. Helper Method Added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_reserve  # Helper Method created
+
+  # Current Reservation function
+  def current_reserve
+    if !session[:order_id].nil? # Check for Reservation session existance
+      Reserve.find(session[:order_id])  # Find reservation session
+    else
+      Reserve.new # Else, create new Reservation 
+    end
+
+  end
+
 end
