0e776be97c81d7a7c90cba64c346611347db45ad
Andres Morales <mora200217@hotmail.com>
2017-08-06 13:14:17 -0500

    Hot fix - Mandatory

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :elements, only [:index]
-  resource :cart, only [:show]
-  resources :reserve_items, only [:create, :update, :destroy]
+  resources :elements, only: [:index]
+  resource :cart, only: [:show]
+  resources :reserve_items, only: [:create, :update, :destroy]
 
   root "elements#index"
 
