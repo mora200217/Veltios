9aea4bd4706de8a0297cafa0cbb5331c9389d9e7
Andres Morales <mora200217@hotmail.com>
2017-08-06 13:11:41 -0500

    Reserve Item Element existance check created

--- app/models/reserve_item.rb
+++ app/models/reserve_item.rb
@@ -1,4 +1,13 @@
 class ReserveItem < ApplicationRecord
+  # Main Initial Associations
   belongs_to :element
   belongs_to :order
+
+  # Function Element Present
+  def element_present
+    # Check for Element Existance
+    if element.nil?
+      errors.add(:element, "Is not a valid element or is inactive") # Display Erros Message
+    end
+  end
 end
