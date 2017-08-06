d62c6f677c969b3b49e4ea20b77df3e349be5cc9
Andres Morales <mora200217@hotmail.com>
2017-08-06 13:07:53 -0500

    Defined Associations and limited scope of Elements model

--- app/models/element.rb
+++ app/models/element.rb
@@ -1,5 +1,9 @@
 class Element < ApplicationRecord
   # Accesible attributes for Element Registers
     attr_accesible :amount, :available
-    
+
+  # Main Initaial Associations
+  has_many :reserve_items
+
+  default scope {where(active: true)}
 end
