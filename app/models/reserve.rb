b2b70f8f80b8ae31398d0f09cfd10cfcf99f67b4
Andres Morales <mora200217@hotmail.com>
2017-08-06 12:16:10 -0500

    Reserve Model Changed. Associations and setter done

--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -1,2 +1,12 @@
 class Reserve < ApplicationRecord
+  # Main Initial Associations
+  has_many :reserve_items
+  belongs_to :reserve_status
+
+  # Set Reservation Status
+  def set_reserve_status
+    self.reserve_status_id = 1 
+  end
+
+
 end
