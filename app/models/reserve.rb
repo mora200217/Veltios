b817b3cb2ba5b945deb3bcb4bcad3a87a6eb651b
Andres Morales <mora200217@hotmail.com>
2017-08-06 16:47:40 -0500

    added before_validation helper Reserve Model

--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -3,6 +3,9 @@ class Reserve < ApplicationRecord
   has_many :reserve_items
   belongs_to :reserve_status
 
+  before_validation :set_order_status # Invoke function on creation
+
+  private
   # Set Reservation Status
   def set_reserve_status
     self.reserve_status_id = 1
