3ec035ab0f11fafa97547715fcf196a389daa752
Andres Morales <mora200217@hotmail.com>
2017-09-05 20:42:35 -0500

    User Model and views from devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :elements, only: [:index]
   resource :cart, only: [:show]
   resources :reserve_items, only: [:create, :update, :destroy]
