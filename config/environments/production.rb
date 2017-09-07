f0b7e632769cf05c74b8744d719c15522f0c115f
Andres Morales <mora200217@hotmail.com>
2017-09-07 17:41:43 -0500

    Production environment mailar config edited

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,24 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 25,
+      # :host => "veltios.herokuapp.com"
+      :domain => "gmail.com",
+      :user_name => "veltiosapp",
+      :password => "VeltiosApp200217",
+      :authentication => "plain",
+      :enable_starttls_auto => true
+  }
+
+
+  #
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_caching = true
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
