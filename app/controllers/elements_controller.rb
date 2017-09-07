5b93e9267891ed238917c7bb9a055473cb342b4c
Andres Morales <mora200217@hotmail.com>
2017-09-07 09:57:45 -0500

    Hotfix, cart system corrected

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -1,7 +1,7 @@
 class ElementsController < ApplicationController
 
   def index
-    @current_elements = Element.all # Access to all elements in db
+    @elements = Element.all # Access to all elements in db
     @reserve_item = current_reserve.reserve_items.new # Access to current reserve items and insert one
   end
 
