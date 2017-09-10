dda309b21fef1fd6962855bfacfb029541dfc032
Andres Morales <mora200217@hotmail.com>
2017-09-09 19:33:50 -0500

    Beta dseign advanced. Main Design an done. Mailer updated

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,11 +1,12 @@
 class ReportMailer < ApplicationMailer
-default from: 'veltiosapp@gmail.com'
+default from: 'Veltios | Modern Lab App <veltiosapp@gmail.com>'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.report_mailer.receipt.subject
   #
-  def receipt
+  def receipt(reserve_items)
+    @reserve_items = reserve_items
     mail(to: 'mora200217@hotmail.com', subject: 'Reserva Exitosa')
   end
 
@@ -14,3 +15,10 @@ default from: 'veltiosapp@gmail.com'
   mail(to: 'mora200217@hotmail.com', subject: 'Bienvenido a Veltios!')
   end
 end
+
+# private
+#
+# def send_mail(parametros, email, asunto)
+#   @no
+#   mail()
+# end
