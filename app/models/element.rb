82211e0fb89b31597b83cbfcae111626ef3a462c
Andres Morales <mora200217@hotmail.com>
2017-09-13 21:26:46 -0500

    Elements and cart list redesigned

--- app/models/element.rb
+++ app/models/element.rb
@@ -5,7 +5,7 @@ class Element < ApplicationRecord
   # Main Initaial Associations
   has_many :reserve_items
   has_attached_file :preview, styles: { medium: "300x300>", thumb: "100x100>" } # , default_url: "/images/:style/missing.png"
-  # validates_attachment_content_type :preview, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :preview, content_type: /\Aimage\/.*\z/
 
   # Define Initial Scope
   default_scope {where(available: true)}
