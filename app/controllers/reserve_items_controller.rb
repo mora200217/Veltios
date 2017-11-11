d01bdac1b3b144880a1f5cea2e7a6d92c3d49e7d
Andrés Morales <mora200217@hotmail.com>
2017-11-11 12:22:32 -0500

    Reserve Items controller modified.Format imrpoved. Comments added and token OAUTH google authorization inserted

--- app/controllers/reserve_items_controller.rb
+++ app/controllers/reserve_items_controller.rb
@@ -4,7 +4,7 @@ class ReserveItemsController < ApplicationController
 
   # Created Method
   def create
-@reserve = current_reserve # Get current reserve grouo
+    @reserve = current_reserve # Get current reserve grouo
     if !@reserve.reserve_items.where(element_id: reserve_items_params[:element_id]).exists?(conditions =  :none)
       @reserve_item = @reserve.reserve_items.new(reserve_items_params) # Create a new item, based in private params function
       @reserve.save
@@ -12,7 +12,7 @@ class ReserveItemsController < ApplicationController
       @element = Element.find(@reserve_item[:element_id])
       @initial_element_amount = @element.amount.to_i # Declare initial amount value of the element
       @element.update_attribute(:amount, @initial_element_amount - @reserve_item[:amount].to_i)
-    # else
+      # else
       # @reserve_item = @reserve.reserve_items.where(reserve_items_params[:element_id])
       # # @reserve_item.first.update_attributes(reserve_items_params)
       # @reserve_items = @reserve.reserve_items
@@ -21,7 +21,7 @@ class ReserveItemsController < ApplicationController
 
 
 
-  redirect_to root_path
+    redirect_to root_path
   end
 
 
@@ -58,8 +58,21 @@ class ReserveItemsController < ApplicationController
 
   # Privates Methods Initializer
   private
+  # Reserve itemps private access methods
   def reserve_items_params
     @params = params.require(:reserve_item).permit(:amount, :element_id) # Params permition changed
     return @params
+  end
+
+  # google api registration OAUTH private method
+  def client_options
+    {
+      client_id: Rails.application.secrets.google_client_id, # Client Id from secrets config file
+      client_secret: Rails.application.secrets.google_client_secret, # Client secret from secrets config file
+      authorization_uri: 'https://accounts.google.com/o/oauth2/auth', # Authentication url for google linkage
+      token_credential_uri: 'https://accounts.google.com/o/oauth2/token', # Authentication url for google linkage
+      scope: Google::Apis::CalendarV3::AUTH_CALENDAR,  # Declaration of initial auth calendar service scope
+      redirect_uri: callback_url # initializate callback url for service appliance (?) 
+    }
   end
 end
