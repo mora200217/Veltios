091192faff7b678a0854dde852a7708f0a71debb
Andrés Morales <mora200217@hotmail.com>
2017-12-19 16:30:48 -0500

    Default image removed

--- app/models/element.rb
+++ app/models/element.rb
@@ -4,7 +4,7 @@ class Element < ApplicationRecord
 
   # Main Initaial Associations
   has_many :reserve_items
-  has_attached_file :preview, styles: { medium: "300x300>", thumb: "100x100>"} , default_url: "missing.png"
+  has_attached_file :preview, styles: { medium: "300x300>", thumb: "100x100>"} #, default_url: "missing.png"
   validates_attachment_content_type :preview, content_type: /\Aimage\/.*\z/
 
   # Define Initial Scope
